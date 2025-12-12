d1d7fa8c836b42547c1eb173d94b27b76d3407da
Tom-Kon <tom.konings@kuleuven.be>
2025-12-12 11:45:04 +0100

    web version

--- shared/src/commonMain/kotlin/com/example/excipientquiz/LocalizedStrings.kt
+++ shared/src/commonMain/kotlin/com/example/excipientquiz/LocalizedStrings.kt
@@ -1,7 +1,7 @@
-package com.example.excipientquiz
-
-import androidx.compose.runtime.Composable
-
-// Common declaration, visible in commonMain
-@Composable
-expect fun localizedStringRuntime(resId: Int, localeCode: String = LocalLocale.current.language): String
+//package com.example.excipientquiz
+//
+//import androidx.compose.runtime.Composable
+//
+//// Common declaration, visible in commonMain
+//@Composable
+//expect fun localizedStringRuntime(resId: Int, localeCode: String = LocalLocale.current.language): String
